42f957106b30e4e923f4828652c5daad091594ef
Ayeshem <ikramayeshem@gmail.com>
2024-03-12 09:22:05 +0500

    mernEstate

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,24 @@
-import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import About from './pages/About';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+
 
 export default function App()
 {
-  return<h1 className='text-red-500'>App</h1>;
+  return( 
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        < Route path ="/home" element={<Home/>} />
+        < Route path ="/sign-in" element={<SignIn/>} />
+        < Route path ="/sign-up" element={<SignUp/>} />
+        < Route path ="/about" element={<About/>} />
+        < Route path ="/profile" element={<Profile/>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
